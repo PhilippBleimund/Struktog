2a77752db358ae94c1fb878c7d4c92ddef4d7f59
schlorge <schlorge@gmail.com>
2023-07-20 11:22:53 +0200

    Added functionality - extra functions for functionButton + caseButton - cleaned some things up

--- test/buttontest.js
+++ test/buttontest.js
@@ -31,10 +31,58 @@ const caseButton = new Button(      'CaseButton',       '/div[1]/div[1]/div[2]/i
 const tryCatchButton = new Button(  'TryCatchButton',   '/div[4]/div[2]/div[2]/input',          '/div[4]/div[2]/div[1]/span',           '/div[1]/div[1]/div[2]',        '/div[2]/div',              '/div[2]/div/div[2]/div',       '/div[2]/div/div/div'       );
 const functionButton = new Button(  'FunctionButton',   '/div[1]/div[2]/input',                 '/div[1]/div[2]/span',                  '/div[1]/div[6]/div/div',       '/div[2]/div',              '/div[2]/div/div[2]/div',       '/div[2]/div/div/div'       );
 
-
 let buttons = [inputButton, outputButton, taskButton, countLoopButton, headLoopButton, footLoopButton, branchButton, caseButton, tryCatchButton, functionButton];
-let delButton, tryCatchText, vtest;
+let vButton, vtest;
 
+//function to check specifically for the parameter of the FunctionButton
+async function functionButtonParameter(driver) { //we can use absolute xPath here, because we're only doing this once
+    await driver.findElement(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/button')).click();
+    await driver.findElement(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/span')).click();
+    await driver.findElement(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/input')).sendKeys('testPar' + Key.RETURN);
+    vtest = await driver.findElement(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/span')).getText();
+    assert.strictEqual(vtest, 'testPar');
+    console.log('Parameter Text Test passed');
+
+    await driver.findElement(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/button')).click();
+    vtest = (await driver.findElements(By.xpath('/html/body/main/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/span'))).length;
+    assert.strictEqual(vtest, 0);
+    console.log('Parameter Deletion Test passed');
+}
+
+//function to check deleting and adding cases via the CaseButton menu
+async function caseButtonMenu(driver, loopPath) { 
+    //click to open case1 text area, put in "test1" and check if true
+    vButton = await driver.findElement(By.xpath(baseX + loopPath + '/div[2]/div[1]/div[1]/div/div[1]/div[1]'));
+    await driver.executeScript("arguments[0].click();", vButton);
+    await driver.findElement(By.xpath(baseX + loopPath + '/div[2]/div[1]/div[1]/div/div[1]/div[2]/input')).sendKeys('test1' + Key.RETURN);
+    vtest = await driver.findElement(By.xpath(baseX + loopPath + '/div[2]/div[1]/div[1]/div/div[1]/div[1]/span')).getText();
+    
+    assert.strictEqual(vtest, 'test1');
+    console.log(' Case1 Text Test passed');
+    
+    //click delete icon for else case and check if case has been deleted
+    vButton = await driver.findElement(By.xpath(baseX + loopPath + '/div[2]/div[3]/div[1]/div/div[2]/div'));
+    await driver.executeScript("arguments[0].click();", vButton);
+    vtest = (await driver.findElements(By.xpath(baseX + loopPath + '/div[2]/div[3]/div[1]/div/div[1]/div[1]/span'))).length;
+    
+    assert.strictEqual(vtest, 0);
+    console.log(' Else Case Deletion Test passed');
+    
+    //click settings buttons, add else back and one extra case and check
+    vButton = await driver.findElement(By.xpath(baseX + loopPath + '/div[1]/div[2]/div[1]'));
+    await driver.executeScript("arguments[0].click();", vButton);
+    await driver.findElement(By.xpath('/html/body/main/div[2]/div[2]/div[2]/div/dl/dd[1]/div[2]')).click();
+    await driver.findElement(By.xpath('/html/body/main/div[2]/div[2]/div[2]/div/dl/dd[2]/div[2]')).click();
+    await driver.findElement(By.xpath('/html/body/main/div[2]/div[2]/div[3]/div')).click();
+    
+    vtest = (await driver.findElements(By.xpath(baseX + loopPath + '/div[2]/div[4]/div[1]/div/div[1]/div[1]/span'))).length;
+    assert.strictEqual(vtest, 1);
+    vtest = (await driver.findElements(By.xpath(baseX + loopPath + '/div[2]/div[3]/div[1]/div/div[1]/div[1]/span'))).length;
+    assert.strictEqual(vtest, 1);
+    console.log(' Case Add Test passed');
+}
+
+//main test function iterating over all buttons/elements and calling itself to achieve deeper nesting level
 async function uiTest(driver, basePath, clickPath, loopClickPath, loopPath, counter) {    
     for (let i = 0; i < buttons.length; i++) {
         if (buttons[i].id == 'FunctionButton' && counter != 0) return; //if current button is FunctionButton and not root -> skip
@@ -50,14 +98,17 @@ async function uiTest(driver, basePath, clickPath, loopClickPath, loopPath, coun
         //click to open text area, put in "test" and check if text is "test"
         await driver.findElement(By.xpath(basePath + clickPath)).click(); 
         if (buttons[i].id == 'TryCatchButton') { //extra click necessary for TryCatchButton to open textarea
-            tryCatchText = await driver.findElement(By.xpath(baseX + loopPath + '/div[4]/div[2]/div[1]')); 
-            await driver.executeScript("arguments[0].click();", tryCatchText);
+            vButton = await driver.findElement(By.xpath(baseX + loopPath + '/div[4]/div[2]/div[1]')); 
+            await driver.executeScript("arguments[0].click();", vButton);
         }
         await driver.findElement(By.xpath(baseX + loopPath + buttons[i].inputX)).sendKeys('test' + Key.RETURN);
         vtest = await driver.findElement(By.xpath(baseX + loopPath + buttons[i].textX)).getText();
 
         assert.match(vtest, /test/);
         console.log(' Text Test passed');
+
+        if (buttons[i].id == 'FunctionButton') await functionButtonParameter(driver); //test parameter for FunctionButton
+        if (buttons[i].id == 'CaseButton') await caseButtonMenu(driver, loopPath); //test menu for CaseButton
 
         //recursive function call depending on type of current element and depth of recursion
         if (counter == 0 && buttons[i].loopX != '') { 
@@ -67,8 +118,8 @@ async function uiTest(driver, basePath, clickPath, loopClickPath, loopPath, coun
         } else {}
             
         //click delete icon and check if element has been deleted (array of applicable elements is empty)
-        delButton = await driver.findElement(By.xpath(baseX + loopPath + buttons[i].deleteX));
-        await driver.executeScript("arguments[0].click();", delButton);
+        vButton = await driver.findElement(By.xpath(baseX + loopPath + buttons[i].deleteX));
+        await driver.executeScript("arguments[0].click();", vButton);
 
         vtest = (await driver.findElements(By.xpath(baseX + loopPath + buttons[i].textX))).length;
 
@@ -77,13 +128,14 @@ async function uiTest(driver, basePath, clickPath, loopClickPath, loopPath, coun
     }
 }
 
+//main function to start a browser and call our test function
 async function selTest() {
     let driver = await new Builder().forBrowser('chrome').build();
     //let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build(); //open Chrome browser headless
     //let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless()).build(); //open Firefox browser headless
     await driver.manage().window().maximize();
     await driver.get('http://127.0.0.1:5500/build/index.html'); //open the website 
-    await driver.manage().setTimeouts({implicit: 200}, {pageLoad: 500}, {script: 500});
+    await driver.manage().setTimeouts({implicit: 100}, {pageLoad: 500}, {script: 100});
     try {
         await uiTest(driver, baseX, '', '', '', 0);
     } finally {
